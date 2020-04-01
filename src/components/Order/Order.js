167a72727d2cbe8133f61bcbf34d86bb0078e64b
Kenneth <kennethvdstrieckt@hotmail.com>
2020-04-01 20:01:00 +0200

    section 13 done

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
+const order = (props) => {
     const ingredients = [];
 
     for(let ingredientName in props.ingredients) {
@@ -26,7 +26,6 @@ const order = (props) => (
             <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     )
-    
-)
+};
 
 export default order;
